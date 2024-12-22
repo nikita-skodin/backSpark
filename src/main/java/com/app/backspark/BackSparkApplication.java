3307589bb3c92a68cb3c5c64c6dd9d41352a6b5b
skodin <skodin36@gmail.com>
2024-12-22 13:33:57 +0300

    Initial commit

---
+++ src/main/java/com/app/backspark/BackSparkApplication.java
@@ -0,0 +1,13 @@
+package com.app.backspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSparkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/backspark/BackSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.backspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
